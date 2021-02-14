762d22656e25ab94bf8425199f9cba5796f2dcbb
Cian <cian@ciangallagher.net>
2021-02-14 23:15:20 +0000

    TEST: Add additional specs for error cases in actions.

--- pkg/gh/actions_test.go
+++ pkg/gh/actions_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/google/go-github/v33/github"
 )
 
-func TestListRecentWorkflows(t *testing.T) {
+func TestWorkflowRuns(t *testing.T) {
 	var (
 		ctx   = context.Background()
 		owner = "Cian911"
@@ -20,19 +20,39 @@ func TestListRecentWorkflows(t *testing.T) {
 	)
 
 	t.Run("It returns a list of recent workflow runs", func(t *testing.T) {
-		runs, _ := c.RecentWorkflowRuns(ctx, owner, repo, opts)
+		runs, _, _ := c.RecentWorkflowRuns(ctx, owner, repo, opts)
 
 		if len(runs.WorkflowRuns) == 0 {
 			t.Errorf("Expected runs count to be greater than 0, but it wasn't: %v", runs)
 		}
 	})
 
+	t.Run("It throws an error when wrong data has been provided", func(t *testing.T) {
+		cc := NewClientWithToken(ctx, "")
+		_, resp, err := cc.RecentWorkflowRuns(ctx, owner, repo, opts)
+		want := 401
+
+		if want != resp.StatusCode {
+			t.Errorf("Expected 401 error code but got something else: %d - %v", resp.StatusCode, err)
+		}
+	})
+
 	t.Run("It returns a single workflow run", func(t *testing.T) {
 		runId := int64(474347037)
-		run, _ := c.WorkflowRunById(ctx, owner, repo, runId)
+		run, _, _ := c.WorkflowRunById(ctx, owner, repo, runId)
 
 		if *run.ID != runId {
 			fmt.Errorf("Expected 1 individual run, got something else: %v", run)
+		}
+	})
+
+	t.Run("It returns a 404 when run does not exist", func(t *testing.T) {
+		runId := int64(11111111)
+		_, resp, err := c.WorkflowRunById(ctx, owner, repo, runId)
+		want := 404
+
+		if resp.StatusCode != want {
+			t.Errorf("Expected a 404, got something else: %d - %v", resp.StatusCode, err)
 		}
 	})
 }
